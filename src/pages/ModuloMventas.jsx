a7350e8117de34f041919ae38afd8869501d1ce7
NatSG14 <natix314@hotmail.com>
2021-09-29 18:42:13 -0500

    pruebas

--- src/pages/ModuloMventas.jsx
+++ src/pages/ModuloMventas.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';yarn 
 
-export const ModuloMventas = () => {
+export function TodoItem({ todo, toggleTodo }) {
+    const { id, task, completed } = todo;
+  
+    const handleTodoClick = () => {
+      toggleTodo(id);
+    };
+  
     return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default ModuloMventas;
+      <li>
+        <input type="checkbox" checked={completed} onChange={handleTodoClick} />
+        {task}
+      </li>
+    );
+  }
