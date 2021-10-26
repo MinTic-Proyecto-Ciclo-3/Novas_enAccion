9030311bedc9094814eeaedae18524452ff9d36a
joalpe13 <joalpe3@gmail.com>
2021-10-25 21:24:13 -0500

    agregué boton a admin

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from 'media/logo copy.png';
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const { loginWithRedirect } = useAuth0();
@@ -15,7 +16,12 @@ const Navbar = () => {
        
       
         <li className='flex space-between px-4'>
-        
+            <Link to='/admin'>
+            <button className='m-2 bg-green-500 p-2 text-white rounded-lg shadow-md hover:bg-indigo-700'>
+              Admin
+            </button>
+            </Link>
+
             <button onClick={() => loginWithRedirect()} 
             className='m-2 bg-green-500 p-2 text-white rounded-lg shadow-md hover:bg-indigo-700'>
               Iniciar Sesion
